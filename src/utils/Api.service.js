a5186ae0bb5c7418f095f016347221421fc9ad51
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-09-28 16:32:03 +0500

    api service

--- src/utils/Api.service.js
+++ src/utils/Api.service.js
@@ -3,3 +3,27 @@ const api= axios.create({
     baseURL:process.env.VITE_API_URL,
     withCredentials:true,
 })
+
+const get = (url, queryParams={})=>{
+const response = api.get(url, queryParams);
+return  response;
+} 
+const post = (url,data)=>{
+    const response = ApiSauceInstance.post(url,data);
+    return response;
+}
+const put = (url,data)=>{
+    const response = ApiSauceInstance.put(url,data);
+    return response;
+}
+const deleteRequest = (url,queryParams)=>{
+     const response =  ApiSauceInstance.delete(url ,queryParams);
+     return response;
+}
+
+export const ApiService= {
+    get,
+    post,
+    put,
+    delete:deleteRequest,
+}
