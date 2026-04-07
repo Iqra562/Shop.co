70bbb348462ad9c7a3060847267fe24af5b43934
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2026-04-07 12:18:35 +0500

    no found ui

--- src/components/common/Products/components/ProductsCard/index.jsx
+++ src/components/common/Products/components/ProductsCard/index.jsx
@@ -1,19 +1,29 @@
 import Card from "@components/common/Card";
 import ProductCardSkeleton from "../ProductCardSkeleton";
+import { FaBasketShopping } from "react-icons/fa6";
+
 function ProductsCard({onsale, limit,useQueryHook }) {
   const { data: productsData, isPending, error } = useQueryHook();
   const getProducts = productsData?.data?.data ?? [];
   const products = limit ? getProducts.slice(0, limit) : getProducts;
+   if (isPending) {
+    return <ProductCardSkeleton />;
+  }
+    if (error || products.length === 0) {
+    return (
+    <div className="w-full flex flex-col justify-center items-center pt-16 ">
+  <FaBasketShopping className="text-9xl text-gray-300" />
+<h2 className="text-gray-300 text-2xl md:text-4xl font-bold  text-center">No products available right now.</h2></div>
+    );
+  }
 
   return (
     <>
-       {isPending ? (
-        <ProductCardSkeleton />
-      ) : (
+     
         <div className=" grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-y-8 gap-x-3 md:gap-y-8 md:gap-x-4">
           {products.map((data, i) => (
             <Card
-              key={i}
+              key={data._id}
               id={data._id}
               productName={data.name}
               description={data.description}
@@ -24,7 +34,7 @@ function ProductsCard({onsale, limit,useQueryHook }) {
             />
           ))}
         </div>
-      )}
+    
     </>
   );
 }
--- src/features/public/shopByCategory/components/Banner/index.jsx
+++ src/features/public/shopByCategory/components/Banner/index.jsx
@@ -3,7 +3,7 @@ function Banner(){
 <h1>
    <div className="border-b py-4 space-y-3">
         <h1 className="text-4xl text-primary font-extrabold uppercase">Products</h1>
-        < p className="w-5/12 text-base text-gray-600 font-semibold">Explore our wide range of high-quality products designed to meet your everyday needs with style and reliability.</p>
+        < p className=" lg:w-5/12 text-base text-gray-600 font-semibold">Explore our wide range of high-quality products designed to meet your everyday needs with style and reliability.</p>
       </div></h1>
     )
 }
