94691df25f950b3a6a8bd50fb613a190e1675c70
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2026-04-12 00:11:25 +0500

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
   plugins: [react()],
-   base: "./", 
+   base: "/", 
  build: {
  outDir: "dist",
  },
